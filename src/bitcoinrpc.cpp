f9ac29769280eb0ddb6b197b6553aaff1501023e
sparklecoin <dev@sparklecoin.com>
2020-03-18 07:54:07 +0000

    remove time conversion in getblock

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -369,7 +369,7 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool fTxI
     result.push_back(Pair("height", blockindex->nHeight));
     result.push_back(Pair("version", block.nVersion));
     result.push_back(Pair("merkleroot", block.hashMerkleRoot.GetHex()));
-    result.push_back(Pair("time", DateTimeStrFormat(block.GetBlockTime())));
+    result.push_back(Pair("time", (int64_t)(block.GetBlockTime())));
     result.push_back(Pair("nonce", (boost::uint64_t)block.nNonce));
     result.push_back(Pair("bits", HexBits(block.nBits)));
     result.push_back(Pair("difficulty", GetDifficulty(blockindex)));
